8e3c7e609e93b8f8266c6321d9464c0ebfd9eba7
milaq <micha.laqua@gmail.com>
2012-05-02 22:42:19 +0200

    add a few missing libs until they can safely be built from source

--- leo-vendor-blobs.mk
+++ leo-vendor-blobs.mk
@@ -20,29 +20,36 @@ PRODUCT_COPY_FILES := \
 
 # All the blobs necessary for leo
 PRODUCT_COPY_FILES += \
-    vendor/htc/leo/proprietary/akmd:system/bin/akmd \
-    vendor/htc/leo/proprietary/parse_radio_log:system/bin/parse_radio_log \
+	vendor/htc/leo/proprietary/akmd:system/bin/akmd \
+	vendor/htc/leo/proprietary/parse_radio_log:system/bin/parse_radio_log \
 	vendor/htc/leo/proprietary/AdieHWCodecSetting.csv:system/etc/AdieHWCodecSetting.csv \
-    vendor/htc/leo/proprietary/AudioBTID.csv:system/etc/AudioBTID.csv \
-    vendor/htc/leo/proprietary/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
-    vendor/htc/leo/proprietary/default.acdb:system/etc/firmware/default.acdb \
-    vendor/htc/leo/proprietary/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    vendor/htc/leo/proprietary/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/htc/leo/proprietary/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/leo/proprietary/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
-    vendor/htc/leo/proprietary/libhtc_ril.so:system/lib/libhtc_ril.so \
-    vendor/htc/leo/proprietary/libhtc_ril_wrapper.so:system/lib/libhtc_ril_wrapper.so \
+	vendor/htc/leo/proprietary/AudioBTID.csv:system/etc/AudioBTID.csv \
+	vendor/htc/leo/proprietary/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+	vendor/htc/leo/proprietary/default.acdb:system/etc/firmware/default.acdb \
+	vendor/htc/leo/proprietary/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+	vendor/htc/leo/proprietary/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+	vendor/htc/leo/proprietary/libgsl.so:system/lib/libgsl.so \
+	vendor/htc/leo/proprietary/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
+	vendor/htc/leo/proprietary/liblasic.so:system/lib/liblasic.so \
+	vendor/htc/leo/proprietary/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
+	vendor/htc/leo/proprietary/libhtc_ril.so:system/lib/libhtc_ril.so \
+	vendor/htc/leo/proprietary/libhtc_ril_wrapper.so:system/lib/libhtc_ril_wrapper.so \
 	vendor/htc/leo/proprietary/libsc-a2xx.so:system/lib/libsc-a2xx.so
 
 # Camera
 PRODUCT_COPY_FILES += \
-    vendor/htc/leo/proprietary/libcamera.so:system/lib/libcamera.so \
-    vendor/htc/leo/proprietary/liboemcamera.so:system/lib/liboemcamera.so
+	vendor/htc/leo/proprietary/libcamera.so:system/lib/libcamera.so \
+	vendor/htc/leo/proprietary/liboemcamera.so:system/lib/liboemcamera.so
+	
+# omxtest
+PRODUCT_COPY_FILES += \
+	vendor/htc/leo/proprietary/mm-vdec-omx-test:system/bin/mm-vdec-omx-test \
+	vendor/htc/leo/proprietary/mm-venc-omx-test:system/bin/mm-venc-omx-test
 
 # HW Acceleration
 PRODUCT_COPY_FILES += \
 	vendor/htc/leo/proprietary/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    vendor/htc/leo/proprietary/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-    vendor/htc/leo/proprietary/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-    vendor/htc/leo/proprietary/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/leo/proprietary/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so
+	vendor/htc/leo/proprietary/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
+	vendor/htc/leo/proprietary/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+	vendor/htc/leo/proprietary/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+	vendor/htc/leo/proprietary/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so
